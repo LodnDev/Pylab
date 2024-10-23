b78a80c7fb9302c20d9ac1d3025f2acd79538f85
LodnDev <lucasoliveiradonascimento8@gmail.com>
2024-10-23 17:19:33 -0400

    Entrando em todos os Sites

--- Bot_Login/login.py
+++ Bot_Login/login.py
@@ -1,8 +1,11 @@
 import getpass
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-import pyautogui as pg
-from time import sleep
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 def logar(usuario, senha, campoNome, campoSenha, botao):
     if campoNome == "" and campoSenha == "" or campoNome == "" or campoSenha == "":
@@ -44,16 +47,33 @@ logar(matricula, senhaAluno, "", "", "btn.btn-primary")
 sleep(1.5)
 navegador.execute_script("window.open('https://github.com/login', '_blank');")
 navegador.switch_to.window(navegador.window_handles[2])
+
+campo = navegador.find_element(By.XPATH, '//*[@id="login_field"]')
+campo.send_keys('')
+campo = navegador.find_element(By.XPATH, '//*[@id="password"]')
+campo.send_keys('')
+botao = navegador.find_element(By.XPATH, '//*[@id="login"]/div[4]/form/div/input[13]')
+botao.click()
 sleep(2.0)
-
-
 
 #Entra na Cisco
 sleep(1.5)
-navegador.execute_script("window.open('https://www.netacad.com/', '_blank');")
+navegador.execute_script("window.open('https://auth.netacad.com/auth/realms/skillsforall/protocol/openid-connect/auth?client_id=b2e-marketplace&redirect_uri=https%3A%2F%2Fwww.netacad.com%2Fdashboard&state=4f799b7a-ea49-4c86-87fe-fbaa71abf986&response_mode=fragment&response_type=code&scope=openid&nonce=ffe7d13d-18a2-44dd-8ed8-091eec87ba05&ui_locales=en-US', '_blank');")
 navegador.switch_to.window(navegador.window_handles[3])
-sleep(1.5)
-btLogin = navegador.find_element(By.CLASS_NAME, "btn btn--ghost.loginBtn--lfDa2")
 
+try:
+    campo = WebDriverWait(navegador, 150).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="username"]'))
+    )
+    campo.send_keys('')
+    campo.send_keys(Keys.ENTER)
+
+    campo = WebDriverWait(navegador, 150).until(
+        EC.visibility_of_element_located((By.XPATH, '//*[@id="password"]'))
+    )
+    campo.send_keys('')
+    campo.send_keys(Keys.ENTER)
+except TimeoutException:
+    print(f'ERRO: O Tempo de Espera foi excedido {TimeoutException}')
 
 fechar = input("Clique Enter para fechar")
